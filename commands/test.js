f325ad613ff66dd42041df07630083b932eaa5cb
khemingkapat <khemingkapat@gmail.com>
2021-05-15 14:43:46 +0700

    change to no output

--- commands/test.js
+++ commands/test.js
@@ -2,21 +2,6 @@ module.exports = {
     name : 'test',
     desc : 'Testing Command',
     execute(message,args){
-        switch(args[1]){
-            case '1':
-                message.reply('1')
-                break
-            case '2':
-                message.reply('2')
-                break
 
-            case 'case':
-                console.log(['covid','light','5'].slice(1,3).sort())
-                break
-            default:
-                message.reply('default')
-                break
-
-        }
     }
 }
