3441f2e400a4d38016f988ae6a347e1279ce2fe9
khemingkapat <khemingkapat@gmail.com>
2021-05-15 14:56:23 +0700

    added link to source file

--- commands/source.js
+++ commands/source.js
@@ -1,7 +1,19 @@
+const {MessageEmbed}  = require('discord.js')
+
 module.exports = {
     name : 'source',
     desc : 'Reply Source Of This Bot',
     execute(message,args){
-        message.reply('This Is Source')
+        const Embed = new MessageEmbed()
+        .setColor('#5DBB63')
+        .setTitle('Source')
+        .setDescription('All links')
+        .addFields(
+            {name:'Covid Data',value:'http://covid19.th-stat.com/api/open/today'},
+            {name:'Github Repository',value:'https://github.com/KhemIngkapat/2021CovidBot'}
+
+
+        )
+        message.reply(Embed)
     }
 }
